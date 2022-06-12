16612837be04b1bf1be0b07be8658be8ef09557c
Hiro Kirigaya <privz286@gmail.com>
2022-06-12 16:54:06 -0300

    :smile: Created DB and Saved User

--- next.config.js
+++ next.config.js
@@ -3,8 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     STRIP_API_KEY: process.env.STRIP_API_KEY,
+    NEXT_PUBLIC_STRIPE_PUBLIC_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY,
+    STRIPE_SUCCESS_URL: process.env.STRIPE_SUCCESS_URL,
+    STRIPE_CANCEL_URL: process.env.STRIPE_CANCEL_URL,
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    FAUNADB_KEY: process.env.FAUNADB_KEY,
+    SIGNING_KEY: process.env.SIGNING_KEY,
   }
 }
 
